5205decb1d9e9476fdbcdfd68edc86fe82d99a52
mrharsh1 <mrharsh1t130921@gmail.com>
2023-07-22 12:33:12 +0530

    script.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3500;
 app.use(express.static('public'));
